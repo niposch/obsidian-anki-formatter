7f93ee324da79d61fdcda9cd6f4facbbfe291734
niposch <niposch@gmail.com>
2023-06-08 13:05:17 +0200

    added unit tests, started implementing some new features

--- o2a.py
+++ o2a.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 strings_to_replace = [
+
     ("\$\$(.+?)\$\$" , "\[.\]"), # mathjax block
     ("\$(.+?)\$" , "\(.\)"), # mathjax inline
     # uncomment the following lines to convert input to anki latex 
@@ -9,7 +10,9 @@ strings_to_replace = [
 
     ("\[\[([^|]+?)\]\]", "."), # obsidian link
     ("\[\[.+?\|(.+?)\]\]", "."), # obsidian link with alias
-    (">.+?", "."), # get rid of blockquotes
+    ("^> *?(.+)", "."), # get rid of blockquotes
+    ("\{\{", "{ {"), # get rid of double curlies
+    ("\}\}", "} }"), # get rid of double curlies
 ]
 import re
 
@@ -24,6 +27,9 @@ def extractor_factory(prefix:str, postfix:str)->callable:
 
 def replace_all(inp, strings_to_replace):
     for replace in strings_to_replace:
+        if "." not in replace[1]:
+            inp = re.sub(replace[0], replace[1], inp)
+            continue
         prefix, postfix = replace[1].split(".")
         inp = re.sub(replace[0], extractor_factory(prefix, postfix), inp)
     return inp
